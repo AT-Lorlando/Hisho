8e7cb516157e3b2f2a916fb9adae90fc22a15e52
AT-Lorlando <rcd.jeremy@gmail.com>
2026-06-10 13:48:57 +0200

    :wrench: Review eco system config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,27 +3,19 @@ module.exports = {
     {
       name: 'hisho-backend',
       script: './build/bin/server.js', 
-      cwd: './backend', // "Current Working Directory" : on se place dans le dossier backend
+      cwd: './backend',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-      },
     },
     {
       name: 'hisho-frontend',
-      // Point d'entrée de Nuxt 3 (moteur Nitro)
       script: './.output/server/index.mjs',
-      cwd: './frontend', // On se place dans le dossier frontend
+      cwd: './frontend',
       instances: 1,
       autorestart: true,
       watch: false,
-      env: {
-        NODE_ENV: 'production',
-        HOST: '0.0.0.0',
-      },
     },
   ],
 };
